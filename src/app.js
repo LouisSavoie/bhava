7e0835796f7c85e0180126890aae5f67f7937384
Louis Savoie <thelousavoie@gmail.com>
2022-12-20 21:30:40 -0500

    Setup command registering
    Adds 2 arrays for testServerCommands and globalCommands and an async
    function registerCommands that registers the commands in the 2 arrays
    with their respective api put requests.

--- src/app.js
+++ src/app.js
@@ -1,15 +1,56 @@
 // IMPORTS
 import * as dotenv from 'dotenv'
-import { Client } from 'discord.js'
+import { Client, REST, Routes } from 'discord.js'
 
 // CONFIG ENV VARS
 dotenv.config()
 
-const client = new Client({ intents: ['Guilds', "GuildMessages"] })
+const client = new Client({ intents: ['Guilds', 'GuildMessages'] })
+const rest = new REST({ version: '10' }).setToken(process.env.BHAVA_TOKEN)
 
-//  LOGIN
+// COMMANDS
+const testServerCommands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+]
+const globalCommands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+]
+
+// REGISTER COMMANDS
+async function registerCommands() {
+  // register test server commands
+  try {
+    console.log('Started refreshing test server (/) commands.')
+
+    await rest.put(Routes.applicationGuildCommands(process.env.BHAVA_APPID, process.env.BHAVA_TESTSERVERID), { body: testServerCommands })
+
+    console.log('Successfully reloaded test server (/) commands.')
+  } catch (error) {
+    console.error(error)
+  }
+  // register global commands
+  try {
+    console.log('Started refreshing global (/) commands.')
+
+    await rest.put(Routes.applicationCommands(process.env.BHAVA_APPID), { body: globalCommands })
+
+    console.log('Successfully reloaded global (/) commands.')
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+registerCommands()
+
+// LOG LOGIN
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`)
+  console.log(`Logged in as ${client.user.tag}!`)
 })
 
 // LOGIN
