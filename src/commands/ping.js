deeb1589ef35d5bcecc7794c65f441ecc8957f17
Louis Savoie <thelousavoie@gmail.com>
2022-12-21 19:03:11 -0500

    Refactor ping command
    Refactors ping command to follow the new pattern of including responses
    in the command files.

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -2,3 +2,15 @@ export const ping = {
   name: 'ping',
   description: 'Responds with pong if the bot is healthy'
 }
+
+export const pingRes = {
+  async execute (interaction) {
+    const username = interaction.member.user.username
+    const guildname = interaction.member.guild.name
+    const guildid = interaction.member.guild.id
+
+    console.log(`${username} on ${guildname} (${guildid}) used (/) command ping`)
+
+    await interaction.reply('Pong!')
+  }
+}
