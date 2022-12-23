1bbda6b5bb77d9b8f3f0fbe961fbb55296f60247
Louis Savoie <thelousavoie@gmail.com>
2022-12-22 23:01:05 -0500

    Remove logging from ping
    Removes the logging code from the ping command which is now deprecated.

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -5,12 +5,6 @@ export const ping = {
 
 export const pingRes = {
   async execute (interaction) {
-    const username = interaction.member.user.username
-    const guildname = interaction.member.guild.name
-    const guildid = interaction.member.guild.id
-
-    console.log(`${username} on ${guildname} (${guildid}) used (/) command ping`)
-
     await interaction.reply('Pong!')
   }
 }
