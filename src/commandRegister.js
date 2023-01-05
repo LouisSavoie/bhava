d0800eef9a1331eea905ba3e00bf5e69689d7653
Louis Savoie <67658723+LouisSavoie@users.noreply.github.com>
2023-01-05 13:18:22 -0500

    Update commandRegister for dev server
    Updates the command register to register commands for the development server apart from the test server for the closed test.

--- src/commandRegister.js
+++ src/commandRegister.js
@@ -1,5 +1,5 @@
 import { REST, Routes } from 'discord.js'
-import { globalCommands, testServerCommands } from './index.js'
+import { globalCommands, testServerCommands, devServerCommands } from './index.js'
 import { log } from './modules/log.js'
 import * as dotenv from 'dotenv'
 dotenv.config()
@@ -7,6 +7,14 @@ dotenv.config()
 const rest = new REST({ version: '10' }).setToken(process.env.BHAVA_TOKEN)
 
 export async function registerCommands () {
+// register dev server commands
+  try {
+    log.registerCommands('Registering', 'dev server')
+    await rest.put(Routes.applicationGuildCommands(process.env.BHAVA_APPID, process.env.BHAVA_DEVSERVERID), { body: devServerCommands })
+    log.registerCommands('Successfully registered', 'dev server')
+  } catch (error) {
+    console.error(error)
+  }
   // register test server commands
   try {
     log.registerCommands('Registering', 'test server')
