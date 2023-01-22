a78b4b302a6d9a2eb760fca344334c7fc3e09220
Louis Savoie <thelousavoie@gmail.com>
2023-01-22 13:21:04 -0500

    Add calcDamage to combat module
    Adds a calcDamage helper function to the combat module, which calculates
    and returns an int damage based on a chars weapon damage and their
    opponents armor value.

--- src/modules/rpg/combat.js
+++ src/modules/rpg/combat.js
@@ -6,6 +6,11 @@ const CRITPERCENT = .25
 const BASEINITIATIVE = 25
 const INITIATIVEDIVIDER = 2
 
+function calcInitiative(armorSkill, maxArmorValue) {
+  const initiative = BASEINITIATIVE + ((Math.floor(armorSkill/INITIATIVEDIVIDER) * 100) - maxArmorValue)
+  return initiative
+}
+
 function calcToHit(weaponSkill) {
   const totalHit = 100 - (BASEHIT - Math.floor(weaponSkill / HITDIVIDER) * 100)
   const toCrit = Math.floor(totalHit * CRITPERCENT)
@@ -14,11 +19,17 @@ function calcToHit(weaponSkill) {
   return [toHit, toCrit]
 }
 
-function calcInitiative(armorSkill, maxArmorValue) {
-  const initiative = BASEINITIATIVE + ((Math.floor(armorSkill/INITIATIVEDIVIDER) * 100) - maxArmorValue)
-  return initiative
+function calcDamage(weapon, armor) {
+  const weaponDamage = Math.floor(roll(weapon.damageMin, weapon.damageMax) * weapon.durability)
+  const armorMitigation = Math.floor(roll(armor.armorValueMin, armor.armorValueMax) * armor.durability)
+  const damage =  weaponDamage - armorMitigation
+  return damage
 }
 
 function rollHit() {
   return Math.floor(Math.random() * 100) + 1
+}
+
+function roll(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min
 }
