562ff55d8ef998c6551ddc895d9f574f78efdd14
Louis Savoie <thelousavoie@gmail.com>
2023-01-21 21:00:35 -0500

    Add helpers to combat module
    Adds 2 helper functions to the combat module. calcInitiative, which
    calculates and returns an int for initiative based on armorSkill and
    an armor penatly equal to maxArmorValue to be compared to another
    initiatve to determine turn order. rollHit, which returns a random int
    between 1 and 100. The consts from calcToHit have also moved out to be
    global and 2 new ones added. calToHit has been corrected for typos.

--- src/modules/rpg/combat.js
+++ src/modules/rpg/combat.js
@@ -1,13 +1,24 @@
 export const combat = {}
 
-function calcToHit(weaponSkill) {
-  const BASEHIT = 50
-  const HITDIVIDER = 2
-  const CRITPERCENT = .25
+const BASEHIT = 50
+const HITDIVIDER = 2
+const CRITPERCENT = .25
+const BASEINITIATIVE = 25
+const INITIATIVEDIVIDER = 2
 
-  const totalHit = 100 - (BASEHIT - Math.floor((weaponSkill / HITDIVIDER) * 100))
-  const toCrit = totalHit * CRITPERCENT
+function calcToHit(weaponSkill) {
+  const totalHit = 100 - (BASEHIT - Math.floor(weaponSkill / HITDIVIDER) * 100)
+  const toCrit = Math.floor(totalHit * CRITPERCENT)
   const toHit = totalHit - toCrit
 
   return [toHit, toCrit]
+}
+
+function calcInitiative(armorSkill, maxArmorValue) {
+  const initiative = BASEINITIATIVE + ((Math.floor(armorSkill/INITIATIVEDIVIDER) * 100) - maxArmorValue)
+  return initiative
+}
+
+function rollHit() {
+  return Math.floor(Math.random() * 100) + 1
 }
